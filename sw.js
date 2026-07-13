9b56270bf399febf525bb560cb56209fb638d5c7
Song Yangyu <flyfy1@gmail.com>
2026-07-14 01:09:34 +0800

    Deploy Employee prototype

---
+++ sw.js
@@ -0,0 +1,8 @@
+const CACHE = "intg-employee-static-v1";
+self.addEventListener("install", () => self.skipWaiting());
+self.addEventListener("activate", (event) => event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key)))).then(() => self.clients.claim())));
+self.addEventListener("fetch", (event) => {
+  const url = new URL(event.request.url);
+  if (event.request.method !== "GET" || url.origin !== self.location.origin || !url.pathname.includes("/assets/")) return;
+  event.respondWith(caches.open(CACHE).then(async (cache) => (await cache.match(event.request)) || fetch(event.request).then((response) => { if (response.ok) cache.put(event.request, response.clone()); return response; })));
+});
